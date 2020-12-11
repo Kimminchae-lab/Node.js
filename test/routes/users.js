aff589446b5e1c97b3fa5e9178f611e037d0d470
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-11 23:33:39 +0900

    users.js

--- test/routes/users.js
+++ test/routes/users.js
@@ -1,19 +1,16 @@
 var express = require("express");
 var router = express.Router();
 
+/* GET users listing. */
 router.post("/", function (req, res, next) {
   if (req.body.cookieName && req.body.selectCookie) {
     if (req.body.selectCookie == "sessionCookie") {
-      res.cookie("Cookie", req.body.cookieName);
-    } else if (req.body.selectCookie == "permanentCookie") {
-      res.cookie("Cookie", req.body.cookieName, {
-        expires: new Date(Date.now() + 10000),
-      });
+      res.cookie("sessionCookie", req.body.cookieName);
+      res.send("<a href='/'>누르시오<a>");
     } else {
-      res.send("입력이 올바르지 않습니다.");
-      res.end();
+      res.cookie("sessionCookie", req.body.cookieName, { maxAge: 5000 });
+      res.send("<a href='/'>누르시오<a>");
     }
-    res.send("<a href='/'>클릭<a>");
   } else {
     res.send("입력이 올바르지 않습니다.");
   }

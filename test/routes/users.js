6ac2c8dde9017e2607d76a884bb64c9e81e051ab
Kimminchae-lab <rlaalsco1022@gmail.com>
2020-12-11 23:31:48 +0900

    Revert "Revert "index.js""
    This reverts commit 599d458d60313e07320898a440ba3b106997c743.

--- test/routes/users.js
+++ test/routes/users.js
@@ -1,16 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
 router.post("/", function (req, res, next) {
   if (req.body.cookieName && req.body.selectCookie) {
     if (req.body.selectCookie == "sessionCookie") {
-      res.cookie("sessionCookie", req.body.cookieName);
-      res.send("<a href='/'>누르시오<a>");
+      res.cookie("Cookie", req.body.cookieName);
+    } else if (req.body.selectCookie == "permanentCookie") {
+      res.cookie("Cookie", req.body.cookieName, {
+        expires: new Date(Date.now() + 10000),
+      });
     } else {
-      res.cookie("sessionCookie", req.body.cookieName, { maxAge: 5000 });
-      res.send("<a href='/'>누르시오<a>");
+      res.send("입력이 올바르지 않습니다.");
+      res.end();
     }
+    res.send("<a href='/'>클릭<a>");
   } else {
     res.send("입력이 올바르지 않습니다.");
   }
